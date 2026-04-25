49cd7d283a540f57c92d2489948ce08180ec75ce
MDT Admin <admin@mendigotoken.com>
2026-04-25 09:33:07 -0600

    fix: asegurar conexion pura a Vercel

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://isdkhmgxnyullvymirwi.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_CTsVCU0cUgkgVJmLWtC-Ng_lipjBKIg';
+// Conexión forzada y pura para evitar errores de inyección del archivo .env en entornos Vercel
+const supabaseUrl = 'https://isdkhmgxnyullvymirwi.supabase.co';
+const supabaseAnonKey = 'sb_publishable_CTsVCU0cUgkgVJmLWtC-Ng_lipjBKIg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
